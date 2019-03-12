b9f52b3ac92b75224b3e7ab97445d93f12a756fd
IN-BR-005 <IN-BR-005@IN-BR-005>
2019-03-12 15:44:36 +0100

    mm

--- Projet_01_I_Commerce/src/fr/adaming/Dao/ISurfDao.java
+++ Projet_01_I_Commerce/src/fr/adaming/Dao/ISurfDao.java
@@ -2,9 +2,14 @@ package fr.adaming.Dao;
 
 import java.util.List;
 
+import fr.adaming.model.Article;
 import fr.adaming.model.Surf;
 
 public interface ISurfDao {
 	public List<Surf> getListeS ();
+	public Surf addSurf (Surf a);
+	public int deleteSurf (Surf a);
+	public int updateSurf (Surf a);
+	public Surf getSurfById (Surf a);
 
 }
--- Projet_01_I_Commerce/src/fr/adaming/Dao/IWetsuitDao.java
+++ Projet_01_I_Commerce/src/fr/adaming/Dao/IWetsuitDao.java
@@ -6,5 +6,9 @@ import fr.adaming.model.Wetsuit;
 
 public interface IWetsuitDao {
 
-	public List<Wetsuit> getListeWs ();
+	public List<Wetsuit> getListeWetSuit ();
+	public Wetsuit addWetsuit (Wetsuit a);
+	public int deleteWetsuit (Wetsuit a);
+	public int updateWetsuit (Wetsuit a);
+	public Wetsuit getWetsuitById (Wetsuit a);
 }
